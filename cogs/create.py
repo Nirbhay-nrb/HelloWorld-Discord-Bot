c1652b7de74f91279e002ec68c51f1ba03868469
Nirbhay Makhija <nmakhija_be20@thapar.edu>
2022-02-10 14:00:59 +0530

    create channel/category feature added

--- cogs/create.py
+++ cogs/create.py
@@ -8,6 +8,8 @@ class Create(commands.Cog):
     
     @commands.command()
     async def create(self, ctx):
+        # list of categories on the server
+        cats = ctx.message.guild.categories
         # menu of options in embed format
         embed = discord.Embed(title='Choose one from the following:', description='Input the corresponding number', color=0xFFF44F)
         embed.add_field(name='1. **Category**',value='Create a category',inline=False)
@@ -27,7 +29,62 @@ class Create(commands.Cog):
             return
         
         # Proceeding further according to user response
-        await ctx.send('Your response {}'.format(msg.content))
+        # creating a category
+        if msg.content == '1':
+            await ctx.send('Please pick a name!!')
+            try:
+                reply = await self.bot.wait_for('message', check=check, timeout=15)
+                await ctx.guild.create_category(reply.content)
+                await ctx.send('Category created!!')
+            except asyncio.TimeoutError:
+                await ctx.send('Sorry, you didn\'t reply in time!')
+        # creating a text channel
+        elif msg.content == '2':
+            embed = discord.Embed(title='Choose one of the following categories:', description='Input the corresponding number', color=0xFFF44F)
+            i = 1
+            for cat in cats:
+                embed.add_field(name=f'{i}. **{cat.name}**',value=f'{cat.id}',inline=False)
+                i = i+1
+            await ctx.send(embed = embed)
+            try:
+                replyCat = await self.bot.wait_for('message', check=check, timeout=10)
+                if int(replyCat.content)-1 >= len(cats):
+                    await ctx.send('Invalid input')
+                    return
+                await ctx.send('Please pick a name!!')
+                try:
+                    replyName = await self.bot.wait_for('message', check=check, timeout=15)
+                    await ctx.guild.create_text_channel(f'{replyName.content}', category=cats[int(replyCat.content)-1])
+                    await ctx.send('Text channel created!!')
+                except asyncio.TimeoutError:
+                    await ctx.send('Sorry, you didn\'t reply in time!')
+            except asyncio.TimeoutError:
+                await ctx.send('Sorry, you didn\'t reply in time!')
+        # creating a voice channel
+        elif msg.content == '3':
+            embed = discord.Embed(title='Choose one of the following categories:', description='Input the corresponding number', color=0xFFF44F)
+            i = 1
+            for cat in cats:
+                embed.add_field(name=f'{i}. **{cat.name}**',value=f'{cat.id}',inline=False)
+                i = i+1
+            await ctx.send(embed = embed)
+            try:
+                replyCat = await self.bot.wait_for('message', check=check, timeout=10)
+                if int(replyCat.content)-1 >= len(cats):
+                    await ctx.send('Invalid input')
+                    return
+                await ctx.send('Please pick a name!!')
+                try:
+                    replyName = await self.bot.wait_for('message', check=check, timeout=15)
+                    await ctx.guild.create_voice_channel(f'{replyName.content}', category=cats[int(replyCat.content)-1])
+                    await ctx.send('Voice channel created!!')
+                except asyncio.TimeoutError:
+                    await ctx.send('Sorry, you didn\'t reply in time!')
+            except asyncio.TimeoutError:
+                await ctx.send('Sorry, you didn\'t reply in time!')
+        # no option 
+        else:
+            await ctx.send('Invalid input')
 
 def setup(client):
     client.add_cog(Create(client))
